d5dcc2e6dcf1ed8daaeaac8c9e83dd9010143722
Nathanthanthan <nathan.petron@yahoo.com>
2024-11-22 16:39:26 +0100

    build: :heavy_plus_sign: added Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+// Tailwind / general styling
+import './index.css';
+
 /**
  * Ionic Dark Mode
  * -----------------------------------------------------
