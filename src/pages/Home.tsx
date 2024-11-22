2745c2bb438c8380fc82433cc07df4020736edb4
Nathanthanthan <nathan.petron@yahoo.com>
2024-11-22 16:44:53 +0100

    refactor: :recycle: basic setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,29 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import ToDoList from '../components/Index';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Todo list</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+
+			<IonContent fullscreen>
+				<IonHeader collapse="condense">
+					<IonToolbar>
+						<IonTitle size="large">
+							Todo list
+						</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+
+				<ToDoList />
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
