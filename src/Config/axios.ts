772ff2688d9677fd95a5e2951696b0c4bd927798
Nathanthanthan <nathan.petron@yahoo.com>
2025-01-20 20:02:30 +0100

    fix(General): :bug: Whoops, forgot I had my firebase's url in a .env

--- src/Config/axios.ts
+++ src/Config/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const API_CONFIG = {
-  baseURL: process.env.API_URL,
+  // baseURL: process.env.API_URL,
+  baseURL: "https://tasklist-b5266-default-rtdb.europe-west1.firebasedatabase.app/",
   endpoints: {
-    tasks: '/tasks',
+    tasks: "/tasks",
   },
   timeout: 10000,
   retryAttempts: 3,
@@ -13,6 +14,6 @@ const axiosInstance = axios.create({
   baseURL: API_CONFIG.baseURL,
   timeout: API_CONFIG.timeout,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
